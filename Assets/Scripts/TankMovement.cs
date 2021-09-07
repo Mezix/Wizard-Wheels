cb90c8a8b906131c61d635bb0f9b6d1f8d9558b4
Felix Swimmer <mezteenx@googlemail.com>
2021-09-07 18:30:54 +0200

    Added basic movement and turret that tracks the mouse

---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    public float _movespeed = 0.1f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * _movespeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * _movespeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * _movespeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * _movespeed;
+        }
+    }
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public float RotationSpeed = 5f;
+    private Vector3 rotationVector;
+    void FixedUpdate()
+    {
+        PointTurretAtPosSmoothly(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+    private void PointTurretAtPosSmoothly(Vector3 pos)
+    {
+        //  find the desired angle to face the mouse
+        float zRotToMouse = Mathf.Rad2Deg * Mathf.Atan2(pos.y-transform.position.y, pos.x - transform.position.x); //TODO: make this a helper method!
+
+        //  get closer to the angle with our max rotationspeed
+        float zRotActual = 0;
+        float diff = zRotToMouse - transform.rotation.eulerAngles.z;
+        if (diff < -180) diff += 360;
+
+        if (Mathf.Abs(diff) > RotationSpeed)
+        {
+            zRotActual = transform.rotation.eulerAngles.z + Mathf.Sign(diff) * RotationSpeed;
+        }
+        else
+        {
+            zRotActual = zRotToMouse;
+        }
+        //print(transform.rotation.z);
+
+        //  rotate to this newly calculate angle
+        Quaternion q = new Quaternion();
+        q.eulerAngles = new Vector3(0, 0, zRotActual);
+        transform.rotation = q;
+    }
+}
