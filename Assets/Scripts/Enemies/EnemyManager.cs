a0064d05d475e04197be1befcc7fe42e7f754d9a
Felix Swimmer <mezteenx@googlemail.com>
2021-10-06 18:49:45 +0200

    added the enemy manager

---
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject _enemyPrefab;
+    private List<GameObject> _enemies = new List<GameObject>();
+
+    private void Update()
+    {
+        
+    }
+    private void Start()
+    {
+        SpawnEnemy();
+    }
+    private void SpawnEnemy()
+    {
+        GameObject tmp = Instantiate(_enemyPrefab);
+        _enemies.Add(tmp);
+        if (PlayerTankController.instance) tmp.transform.position = PlayerTankController.instance.transform.position + new Vector3(10,-5,0);
+        else tmp.transform.position = new Vector3(0, 0, 0);
+    }
+}
