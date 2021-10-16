5100cf959a7f35ce0ad2ba63120e53c146d2e251
Felix Swimmer <mezteenx@googlemail.com>
2021-10-16 13:00:45 +0200

    shift click to move camera offset from currently tracked vehicle

--- Assets/Scripts/GameScene/CameraScript.cs
+++ Assets/Scripts/GameScene/CameraScript.cs
@@ -5,23 +5,56 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public Transform vehicle;
+    public Transform vehicleToTrack;
+    private Vector2 cameraOffset;
+    private Vector3 mouseStartDragPos;
+    private bool movingCameraOffset;
 
+    private void Awake()
+    {
+    }
     private void Start()
     {
-        Events.instance.PlayerTankDestroyed += StopTrackingPlayer;
+        Events.instance.PlayerTankDestroyed += StopTracking;
+        SetTrackedVehicleToPlayer();
     }
-    private void StopTrackingPlayer()
-    {
-        vehicle = null;
-    }
-
+    
     void Update()
     {
-        if(vehicle) TrackTank();
+        if(vehicleToTrack) TrackCurrentTank();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if(Input.GetKeyDown(KeyCode.Mouse2)) mouseStartDragPos = Input.mousePosition; //initially set from where we are draggin
+            if(Input.GetKey(KeyCode.Mouse2)) ChangeCameraOffset(); //dragging motion
+        }
+        else movingCameraOffset = false;
     }
-    private void TrackTank()
+    public void SetTrackedVehicleToPlayer()
     {
-        transform.position = vehicle.position + new Vector3(0,0,-10);
+        vehicleToTrack = References.PlayerGameObject.transform;
+        cameraOffset = Vector3.zero;
+    }
+    public void SetTrackedVehicleToEnemy(Transform tankTransform)
+    {
+        vehicleToTrack = tankTransform;
+        cameraOffset = Vector3.zero;
+    }
+    private void TrackCurrentTank()
+    {
+        transform.position = vehicleToTrack.position + new Vector3(cameraOffset.x, cameraOffset.y, -10);
+    }
+    private void StopTracking()
+    {
+        vehicleToTrack = null;
+    }
+    private void ChangeCameraOffset()
+    {
+        movingCameraOffset = true;
+
+        cameraOffset = new Vector3(cameraOffset.x, cameraOffset.y, 0) + 
+                                   Camera.main.ScreenToWorldPoint(mouseStartDragPos) - 
+                                   Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseStartDragPos = Input.mousePosition;
+        print(cameraOffset);
     }
 }
