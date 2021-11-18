9396f6e59e5696cefd69d59ba10e4aade92d49c2
Felix Swimmer <mezteenx@googlemail.com>
2021-11-19 00:20:38 +0100

    added the ability to *ponder my orb*

--- Assets/Scripts/MenuScene/MainMenuSceneManager.cs
+++ Assets/Scripts/MenuScene/MainMenuSceneManager.cs
@@ -1,16 +1,94 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
 
 public class MainMenuSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private PixelPerfectCamera pixelCam;
+
+    //  Menu
+
+    [SerializeField]
+    private GameObject MainMenuGO;
+
+    [SerializeField]
+    private Button MenuStartButton;
+    [SerializeField]
+    private Button MenuSettingsButton;
+
+    //  Select Screen UI
+
+    [SerializeField]
+    private GameObject SelectScreenGO;
+
+    [SerializeField]
+    private Button LaunchGameButton;
+    [SerializeField]
+    private Button PreviousTankButton;
+    [SerializeField]
+    private Button NextTankButton;
+    [SerializeField]
+    private Text SelectedTankText;
+
+    private void Awake()
+    {
+        InitButtons();
+    }
     private void Start()
+    {
+        ActivateMainMenuUI(true);
+        ActivateSelectScreen(false);
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!MainMenuGO.activeInHierarchy)
+            {
+                ReturnToMainMenu();
+            }
+        }
+    }
+    private void InitButtons()
     {
 
     }
 
-    public void StartGame()
+    public void NextTank()
+    {
+
+    }
+    public void PreviousTank()
+    {
+
+    }
+    public void ShowSelectionScreen()
+    {
+        pixelCam.assetsPPU = 100;
+        ActivateMainMenuUI(false);
+        ActivateSelectScreen(true);
+    }
+
+    private void ActivateMainMenuUI(bool b)
+    {
+        MainMenuGO.SetActive(b);
+    }
+    private void ActivateSelectScreen(bool b)
+    {
+        SelectScreenGO.SetActive(b);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        pixelCam.assetsPPU = 32;
+        ActivateMainMenuUI(true);
+        ActivateSelectScreen(false);
+    }
+    public void LaunchGame()
     {
         StartCoroutine(ShowLoadingScreen());
     }
