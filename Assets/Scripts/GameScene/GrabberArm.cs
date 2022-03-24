0a500ec87305c502f78d350120b80a0ca1d5121f
Felix Swimmer <mezteenx@googlemail.com>
2022-03-24 23:33:38 +0100

    fixed the chain

--- Assets/Scripts/GameScene/GrabberArm.cs
+++ Assets/Scripts/GameScene/GrabberArm.cs
@@ -25,6 +25,7 @@ public class GrabberArm : MonoBehaviour
         {
             _clawScript.transform.parent = _grabberArmCrossbowBody;
             _clawScript.transform.localPosition = Vector3.zero;
+            _clawScript._clawAnimator.transform.localPosition = new Vector3(0.166f, 0, 0);
         }
         RotateToMouse();
         CreateChain();
@@ -33,7 +34,9 @@ public class GrabberArm : MonoBehaviour
     private void CreateChain()
     {
         Sprite chain = Resources.Load("Art/Weapons/chain_link", typeof (Sprite)) as Sprite;
-        DottedLine.DottedLine.Instance.DrawDottedLine(_chainStartingPos.position, _clawScript._clawAnimator.transform.position, Color.white, chain);
+        Vector3 clawStartingPos = _clawScript._clawAnimator.transform.position;
+        clawStartingPos += (_chainStartingPos.position - clawStartingPos).normalized * 0.1f;
+        DottedLine.DottedLine.Instance.DrawDottedLine(_chainStartingPos.position, clawStartingPos, Color.white, chain);
     }
 
     private void RotateToMouse()
@@ -48,6 +51,7 @@ public class GrabberArm : MonoBehaviour
         _clawScript.transform.parent = null;
         _clawScript.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         _clawScript.transform.position = new Vector3(_clawScript.transform.position.x, _clawScript.transform.position.y, 0);
+        _clawScript._clawAnimator.transform.localPosition = Vector3.zero;
     }
     private void ToggleArm()
     {
