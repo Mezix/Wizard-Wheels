6fc167ca79b120f9ac0468e185edbd2293e02142
Felix Swimmer <mezteenx@googlemail.com>
2023-08-04 14:05:21 +0200

    added sfx to game scene buttons

--- Assets/Scripts/AddButtonSFX.cs
+++ Assets/Scripts/AddButtonSFX.cs
@@ -8,9 +8,12 @@ using UnityEngine.UI;
 public class AddButtonSFX : MonoBehaviour
 {
     private Button _buttonToAddSFXTo;
+    private static float pitchVariance;
 
     private void Awake()
     {
+        pitchVariance = 0.1f;
+
         _buttonToAddSFXTo = GetComponent<Button>();
         if (!_buttonToAddSFXTo) return;
 
@@ -28,10 +31,14 @@ public class AddButtonSFX : MonoBehaviour
 
     private void PlayClickSoundEffect()
     {
+        if (!REF.AM) return;
+        REF.AM._clickedSound.pitch = UnityEngine.Random.Range(1 - pitchVariance, 1 + pitchVariance);
         REF.AM._clickedSound.Play();
     }
     private void PlayHoverSoundEffect()
     {
+        if (!REF.AM) return;
+        REF.AM._highlightedSound.pitch = UnityEngine.Random.Range(1 - pitchVariance, 1 + pitchVariance);
         REF.AM._highlightedSound.Play();
     }
 }
